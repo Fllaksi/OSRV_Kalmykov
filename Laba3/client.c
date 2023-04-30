1d5d0e20e4546b569c5701a9aa0ec6f951517662
Калмыков Владимир <131619083+Fllaksi@users.noreply.github.com>
2023-04-30 19:19:20 +0300

    Update client.c

--- Laba3/client.c
+++ Laba3/client.c
@@ -6,15 +6,21 @@
 
 int main()
 {
-    char smsg[30] = "Kalmykov Vladimir I993";
+    char smsg[30];
+    char msg[30];
+    char msg2[30];
     char rmsg[200];
     int coid;
     long serv_pid;
+    int q = 0;
     printf("Prog client , Vvedite PID servera \n");
     scanf("%ld", &serv_pid);
     coid = ConnectAttach(0, serv_pid, 1, 0, 0);
     printf("Connect res %d \n , vvedite soobshenie\n", coid);
-    scanf("%30s",&smsg);
+    scanf("%40s", &msg);
+    fflush(stdin);
+    gets(msg2);
+    sprintf(smsg, "%s%s", msg, msg2);
     printf("Vveli %s \n", smsg);
     if (MsgSend(coid, smsg, strlen(smsg) + 1, rmsg, sizeof(rmsg)) == -1)
     {
